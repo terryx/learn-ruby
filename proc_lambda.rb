892a4ff0249fdc3bdf7626b2fa4dbb410956ae33
TerryX <terryxlife@gmail.com>
2015-08-26 14:59:11 +0800

    difference between proc and lambda

---
+++ proc_lambda.rb
@@ -0,0 +1,27 @@
+def run_block(block)
+  print 'Running a '
+  block.call
+  puts "...done\n"
+end
+
+# def app
+#   run_block lambda {
+#     print 'lambda'; return
+#   }
+#   run_block proc {
+#     print 'proc'; return
+#   }
+# end
+
+def app
+  run_block proc {
+    print 'proc'
+    return
+  }
+  run_block lambda {
+    print 'lambda'
+    return
+  }
+end
+
+app
